8c831fc9e146b3960ecc90217a70c97d40ebb765
jessicasyu <jessicayu@u.northwestern.edu>
2020-08-20 18:12:00 -0400

    Added skeleton for 3D PottsLocations tests

---
+++ test/arcade/env/loc/PottsLocations3DTest.java
@@ -0,0 +1,41 @@
+package arcade.env.loc;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import java.util.*;
+import ec.util.MersenneTwisterFast;
+import arcade.env.loc.Location.Voxel;
+import static arcade.sim.Potts.*;
+
+public class PottsLocations3DTest {
+	@Test
+	public void makeLocation_givenList_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void makeLocations_givenList_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void assignVoxels_randomFraction_updatesTags() {
+		// TODO
+	}
+	
+	@Test
+	public void assignVoxels_noCenterVoxel_updatesTags() {
+		// TODO
+	}
+	
+	@Test
+	public void selectVoxels_maxNumber_updatesTags() {
+		// TODO
+	}
+	
+	@Test
+	public void selectVoxels_minNumber_updatesTags() {
+		// TODO
+	}
+}
