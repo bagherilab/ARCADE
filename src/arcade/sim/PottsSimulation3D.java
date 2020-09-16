7b59469b616fbd162e6c19380ff9332370ede3dd
jessicasyu <jessicayu@u.northwestern.edu>
2020-09-16 17:33:12 -0400

    Updated skeleton for 3D simulation

--- src/arcade/sim/PottsSimulation3D.java
+++ src/arcade/sim/PottsSimulation3D.java
@@ -1,21 +1,66 @@
 package arcade.sim;
 
 import java.util.ArrayList;
-import arcade.agent.cell.Cell;
+import java.util.HashSet;
+import ec.util.MersenneTwisterFast;
+import arcade.agent.cell.*;
+import arcade.env.loc.*;
 import arcade.util.MiniBox;
+import static arcade.sim.Potts.*;
+import static arcade.agent.cell.Cell.*;
 import static arcade.env.loc.Location.*;
 
 public class PottsSimulation3D extends PottsSimulation {
 	public PottsSimulation3D(long seed, Series series) { super(seed, series); }
 	
-	public Potts makePotts() { return new Potts3D(series, agents); }
+	/**
+	 * Converts volume to voxels per square side.
+	 *
+	 * @param volume  the target volume
+	 * @return  the voxels per side
+	 */
+	static int convert(double volume) {
+		// TODO
+		return 0;
+	}
 	
-	ArrayList<arcade.env.loc.Location.Voxel> makeCenters() {
+	/**
+	 * Increases the number of voxels by adding from a given list of voxels.
+	 *
+	 * @param random  the seeded random number generator
+	 * @param allVoxels  the list of all possible voxels
+	 * @param voxels  the list of selected voxels
+	 * @param target  the target number of voxels
+	 */
+	static void increase(MersenneTwisterFast random, ArrayList<Voxel> allVoxels, ArrayList<Voxel> voxels, int target) {
+		// TODO
+	}
+	
+	/**
+	 * Decreases the number of voxels by removing.
+	 *
+	 * @param random  the seeded random number generator
+	 * @param voxels  the list of selected voxels
+	 * @param target  the target number of voxels
+	 */
+	static void decrease(MersenneTwisterFast random, ArrayList<Voxel> voxels, int target) {
+		// TODO
+	}
+	
+	Potts makePotts() { return new Potts3D(series, agents); }
+	
+	ArrayList<int[]> makeCenters() {
+		// TODO
+		
+		return null;
+	}
+	
+	Location makeLocation(MiniBox population, int[] center) {
 		// TODO
 		return null;
 	}
 	
-	Cell makeCell(int id, MiniBox population, Voxel center) {
+	public Cell makeCell(int id, MiniBox population, int[] center) {
 		// TODO
 		return null;
 	}
--- test/arcade/sim/PottsSimulation3DTest.java
+++ test/arcade/sim/PottsSimulation3DTest.java
@@ -1,16 +1,148 @@
 package arcade.sim;
 
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import ec.util.MersenneTwisterFast;
+import arcade.agent.cell.*;
+import arcade.env.loc.*;
+import arcade.util.MiniBox;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static arcade.sim.Potts.*;
+import static arcade.agent.cell.Cell.*;
+import static arcade.env.loc.Location.Voxel;
 import static arcade.sim.PottsSimulationTest.*;
 
 public class PottsSimulation3DTest {
+	private static final double EPSILON = 1E-4;
+	
+	private static final Comparator<int[]> COMPARATOR = (v1, v2) ->
+			v1[2] != v2[2] ? Integer.compare(v1[2], v2[2]) :
+			v1[0] != v2[0] ? Integer.compare(v1[0], v2[0]) :
+					Integer.compare(v1[1], v2[1]);
+	
 	@Test
-	public void setupPotts_anySeries_initializesPotts() {
+	public void makePotts_mockSeries_initializesPotts() {
 		Series series = mock(Series.class);
 		PottsSimulation3D sim = new PottsSimulation3D(RANDOM_SEED, series);
-		sim.setupPotts();
-		assertTrue(sim.potts instanceof Potts3D);
+		Potts potts = sim.makePotts();
+		assertTrue(potts instanceof Potts3D);
+	}
+	
+	@Test
+	public void convert_exactOddCubes_calculateValue() {
+		// TODO
+	}
+	
+	@Test
+	public void convert_exactEvenCubes_calculateValue() {
+		// TODO
+	}
+	
+	@Test
+	public void convert_inexactOddCubes_calculateValue() {
+		// TODO
+	}
+	
+	@Test
+	public void convert_inexactEvenCubes_calculateValue() {
+		// TODO
+	}
+	
+	@Test
+	public void increase_exactTarget_updatesList() {
+		// TODO
+	}
+	
+	@Test
+	public void increase_inexactTarget_updatesList() {
+		// TODO
+	}
+	
+	@Test
+	public void decrease_exactTarget_updatesList() {
+		// TODO
+	}
+	
+	@Test
+	public void decrease_inexactTarget_updatesList() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationOneSideExactEqualSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationOneSideExactUnequalSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationOneSideInexactEqualSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationOneSideInexactUnequalSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationThreeSideExactEqualSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationThreeSideExactUnequalSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationThreeSideInexactEqualSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_onePopulationThreeSideInexactUnequalSize_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCenters_multiplePopulations_createsCenters() {
+		// TODO
+	}
+	
+	@Test
+	public void makeLocation_noTags_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void makeLocation_withTags_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCell_onePopulationNoTags_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCell_multiplePopulationsNoTags_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCell_onePopulationWithTags_createsObject() {
+		// TODO
+	}
+	
+	@Test
+	public void makeCell_multiplePopulationsWithTags_createsObject() {
+		// TODO
 	}
 }
