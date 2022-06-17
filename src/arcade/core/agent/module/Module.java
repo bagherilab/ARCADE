46879f35ed7cc2617bfbf6885fae2f994675ae3f
jessicasyu <jessicayu@u.northwestern.edu>
2022-06-17 13:37:13 -0400

    Added process interface and updated module inferface

--- src/arcade/core/agent/module/Module.java
+++ src/arcade/core/agent/module/Module.java
@@ -4,15 +4,19 @@ import ec.util.MersenneTwisterFast;
 import arcade.core.sim.Simulation;
 
 /**
- * A {@code Module} object is a subcellular steppable.
+ * A {@code Module} object is an exclusive subcellular steppable.
  * <p>
- * {@code Module} objects represent subcellular behaviors or mechanisms within a
- * {@link arcade.core.agent.cell.Cell} object, such as metabolism, signaling
- * networks, and angiogenesis.
- * The {@code Module} should be called during the step method of the corresponding
+ * {@code Module} objects represent subcellular behaviors or mechanisms within
+ * a {@link arcade.core.agent.cell.Cell} object, such as the cell cycle or
+ * cell death.
+ * The {@code Module} should be called during the step method of the
+ * corresponding {@link arcade.core.agent.cell.Cell} object.
+ * A {@code Module} can be implemented with different versions; the specific
+ * class can be selected when switching between {@code Module} instances.
+ * <p>
+ * Only one {@code Module} can be active for a given
  * {@link arcade.core.agent.cell.Cell} object.
- * A {@code Module} can be implemented with different versions; the specific class
- * can be selected when instantiating the {@link arcade.core.agent.cell.Cell} object.
+ * Use {@link arcade.core.agent.process.Process} for non-exclusive steppables.
  */
 
 public interface Module {
