d279f37bd7d89a22551aeb4da266277a85317602
jessicasyu <jessicayu@u.northwestern.edu>
2022-06-17 15:02:54 -0400

    Added additional patch-specific enums

--- src/arcade/patch/util/PatchEnums.java
+++ src/arcade/patch/util/PatchEnums.java
@@ -1,11 +1,15 @@
 package arcade.patch.util;
 
+import ec.util.MersenneTwisterFast;
+
 /**
- * Container class for potts-specific enums.
+ * Container class for patch-specific enums.
  * <p>
  * Implemented enums include:
  * <ul>
  *     <li>{@code Ordering} defining simulation stepping order</li>
+ *     <li>{@code Domain} defining domain for a given module</li>
+ *     <li>{@code Flag} defining state change flags</li>
  * </ul>
  */
 
@@ -21,5 +25,49 @@ public final class PatchEnums {
     public enum Ordering {
         /** Stepping order for cells. */
         CELLS
+    }
+    
+    /** Process domain codes. */
+    public enum Domain {
+        /** Code for undefined domain. */
+        UNDEFINED,
+        
+        /** Code for metabolism domain. */
+        METABOLISM,
+
+        /** Code for signaling domain. */
+        SIGNALING;
+
+        /**
+         * Randomly selects a {@code Domain}.
+         *
+         * @param rng  the random number generator
+         * @return  a random {@code Domain}
+         */
+        public static Domain random(MersenneTwisterFast rng) {
+            return values()[rng.nextInt(values().length - 1) + 1];
+        }
+    }
+    
+    /** State change flags. */
+    public enum Flag {
+        /** Code for undefined flag. */
+        UNDEFINED,
+        
+        /** Code for proliferative flag. */
+        PROLIFERATIVE,
+        
+        /** Code for migratory flag. */
+        MIGRATORY;
+        
+        /**
+         * Randomly selects a {@code Flag}.
+         *
+         * @param rng  the random number generator
+         * @return  a random {@code Flag}
+         */
+        public static Flag random(MersenneTwisterFast rng) {
+            return values()[rng.nextInt(values().length - 1) + 1];
+        }
     }
 }
