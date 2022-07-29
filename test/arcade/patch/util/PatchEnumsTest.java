605c69d14baaa182a88db8650d6d96d74192e8ac
jessicasyu <jessicayu@u.northwestern.edu>
2022-07-29 17:42:40 -0400

    Added unit tests for PatchEnums class

---
+++ test/arcade/patch/util/PatchEnumsTest.java
@@ -0,0 +1,59 @@
+package arcade.patch.util;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import org.junit.Test;
+import ec.util.MersenneTwisterFast;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static arcade.patch.util.PatchEnums.Domain;
+import static arcade.patch.util.PatchEnums.Flag;
+
+public class PatchEnumsTest {
+    @Test(expected = UnsupportedOperationException.class)
+    public void constructor_called_throwsException() {
+        PatchEnums enums = new PatchEnums();
+    }
+    
+    @Test
+    public void Domain_random_returnsDomain() {
+        // Create set of all values.
+        EnumSet<Domain> enumSet = EnumSet.allOf(Domain.class);
+        enumSet.remove(Domain.UNDEFINED);
+        
+        // Create set of all random values.
+        ArrayList<Domain> enumRandom = new ArrayList<>();
+        
+        int n = Domain.values().length - 1;
+        for (int i = 0; i < n; i++) {
+            MersenneTwisterFast rng = mock(MersenneTwisterFast.class);
+            doReturn(i).when(rng).nextInt(n);
+            enumRandom.add(Domain.random(rng));
+        }
+        
+        // Compare resulting sets.
+        EnumSet<Domain> enumSetRandom = EnumSet.copyOf(enumRandom);
+        assertEquals(enumSet, enumSetRandom);
+    }
+    
+    @Test
+    public void Flag_random_returnsFlag() {
+        // Create set of all values.
+        EnumSet<Flag> enumSet = EnumSet.allOf(Flag.class);
+        enumSet.remove(Flag.UNDEFINED);
+        
+        // Create set of all random values.
+        ArrayList<Flag> enumRandom = new ArrayList<>();
+        
+        int n = Flag.values().length - 1;
+        for (int i = 0; i < n; i++) {
+            MersenneTwisterFast rng = mock(MersenneTwisterFast.class);
+            doReturn(i).when(rng).nextInt(n);
+            enumRandom.add(Flag.random(rng));
+        }
+        
+        // Compare resulting sets.
+        EnumSet<Flag> enumSetRandom = EnumSet.copyOf(enumRandom);
+        assertEquals(enumSet, enumSetRandom);
+    }
+}
