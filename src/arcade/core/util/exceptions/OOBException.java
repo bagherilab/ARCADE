75e53ffd31ba8a3442950d22c13afd1dc8d9cb76
cainja <jcain@uw.edu>
2024-10-18 19:07:42 +0000

    added docstrings to OOBException

--- src/arcade/core/util/exceptions/OOBException.java
+++ src/arcade/core/util/exceptions/OOBException.java
@@ -1,6 +1,14 @@
 package arcade.core.util.exceptions;
 
+/**
+ * Exception thrown when a parameter is out of bounds.
+ */
 public class OOBException extends RuntimeException {
+    /**
+     * Constructs an {@code OOBException} with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public OOBException(String message) {
         super(message);
     }
