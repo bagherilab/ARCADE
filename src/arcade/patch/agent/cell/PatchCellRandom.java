7fe734131733fe90a84a659353d01983089bb9ca
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2024-10-21 15:34:47 -0400

    Add population links to cell fields

--- src/arcade/patch/agent/cell/PatchCellRandom.java
+++ src/arcade/patch/agent/cell/PatchCellRandom.java
@@ -5,6 +5,7 @@ import ec.util.MersenneTwisterFast;
 import arcade.core.agent.cell.CellState;
 import arcade.core.env.location.Location;
 import arcade.core.sim.Simulation;
+import arcade.core.util.GrabBag;
 import arcade.core.util.MiniBox;
 import static arcade.patch.util.PatchEnums.Domain;
 import static arcade.patch.util.PatchEnums.State;
@@ -25,16 +26,30 @@ public class PatchCellRandom extends PatchCell {
      * @param parameters the dictionary of parameters
      */
     public PatchCellRandom(PatchCellContainer container, Location location, MiniBox parameters) {
-        super(container, location, parameters);
+        this(container, location, parameters, null);
+    }
+
+    /**
+     * Creates a random {@code PatchCell} agent with population links.
+     *
+     * @param container the cell container
+     * @param location the {@link Location} of the cell
+     * @param parameters the dictionary of parameters
+     * @param links the map of population links
+     */
+    public PatchCellRandom(
+            PatchCellContainer container, Location location, MiniBox parameters, GrabBag links) {
+        super(container, location, parameters, links);
     }
 
     @Override
     public PatchCellContainer make(int newID, CellState newState, MersenneTwisterFast random) {
         divisions--;
+        int newPop = links == null ? pop : links.next(random);
         return new PatchCellContainer(
                 newID,
                 id,
-                pop,
+                newPop,
                 age,
                 divisions,
                 newState,
