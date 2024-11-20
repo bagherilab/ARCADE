93c66d3e586be92e32b806dea0274c81a3fdea5b
allison-li-1016 <allli@uw.edu>
2024-11-20 05:51:10 +0000

    refactor auxin module such that equations are in subclass

--- src/arcade/patch/agent/process/PatchProcessQuorumSensing.java
+++ src/arcade/patch/agent/process/PatchProcessQuorumSensing.java
@@ -1,73 +1,24 @@
 package arcade.patch.agent.process;
 
 import java.util.List;
-
-import arcade.core.sim.Simulation;
 import arcade.patch.agent.cell.PatchCell;
-import arcade.patch.agent.cell.PatchCellCART;
-import ec.util.MersenneTwisterFast;
 
 public abstract class PatchProcessQuorumSensing extends PatchProcess {
 
-    /** ID for auxin. */
-    static final int AUXIN = 0;
+    /** List of amounts of each species */
+    protected double[] concs;
 
-    /** List of internal names. */
-    List<String> names;
+    /** List of internal names */
+    protected List<String> names;
 
-    /** List of internal amounts [fmol]. */
-    double[] intAmts;
+    /** Number of steps per second to take in ODE */
+    private static final double STEP_DIVIDER = 3.0;
 
-    /** List of external amounts [fmol]. */
-    final double[] extAmts;
+    /** Step size for module (in seconds) */
+    static final double STEP_SIZE = 1.0 / STEP_DIVIDER;
 
-    /** List of uptake amounts [fmol]. */
-    final double[] upAmts;
-
-    /** Volume of cell [um<sup>3</sup>]. */
-    double volume;
-
-    /** {@code true} if cell is in activated, {@code false} otherwise. */
-    protected boolean isActive;
-
-
-
-
-     PatchProcessQuorumSensing(PatchCell cell) {
+    PatchProcessQuorumSensing(PatchCell cell) {
         super(cell);
-        volume = cell.getVolume();
-
-        // Initialize external and uptake concentration arrays;
-        extAmts = new double[2];
-        upAmts = new double[2];
-    }
-
-    /**
-     * Steps the metabolism process.
-     *
-     * @param random the random number generator
-     * @param sim the simulation instance
-     */
-    abstract void stepProcess(MersenneTwisterFast random, Simulation sim);
-
-    @Override
-    public void step(MersenneTwisterFast random, Simulation sim) {
-        updateExternal(sim);
-
-        // Check cell state.
-        isActive = ((PatchCellCART)cell).getActivationStatus();
-
-        // Modify internal auxin uptake.
-        stepProcess(random, sim);
-
-        // Update environment.
-        sim.getLattice("AUXIN").updateValue(location, 1.0 - upAmts[AUXIN] / extAmts[AUXIN]);
-
-    }
-
-    private void updateExternal(Simulation sim) {
-        extAmts[AUXIN] =
-                sim.getLattice("AUXIN").getAverageValue(location) * location.getVolume();
     }
 
     /**
@@ -85,5 +36,4 @@ public abstract class PatchProcessQuorumSensing extends PatchProcess {
                 return null;
         }
     }
-    
 }
