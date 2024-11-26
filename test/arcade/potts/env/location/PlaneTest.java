03829ef34f697bde564a050e07a5661d79524074
jannetty <jannetty@uw.edu>
2024-11-26 12:03:59 -0800

    removed redundant import

--- test/arcade/potts/env/location/PlaneTest.java
+++ test/arcade/potts/env/location/PlaneTest.java
@@ -2,7 +2,6 @@ package arcade.potts.env.location;
 
 import org.junit.jupiter.api.Test;
 import sim.util.Double3D;
-import arcade.potts.util.PottsEnums.Direction;
 import static org.junit.jupiter.api.Assertions.*;
 import static arcade.potts.util.PottsEnums.Direction;
 
