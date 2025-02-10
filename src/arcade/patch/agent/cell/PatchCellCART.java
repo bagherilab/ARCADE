6fe1a9775360c57a7438218926d6d244dcc9f27b
allison-li-1016 <allli@uw.edu>
2025-02-10 22:17:48 +0000

    uncommeting WIP comments

--- src/arcade/patch/agent/cell/PatchCellCART.java
+++ src/arcade/patch/agent/cell/PatchCellCART.java
@@ -195,12 +195,12 @@ public abstract class PatchCellCART extends PatchCell {
             case APOPTOTIC:
                 module = new PatchModuleApoptosis(this);
                 break;
-            case CYTOTOXIC:
-                throw new UnsupportedOperationException();
+                //            case CYTOTOXIC:
+                //                throw new UnsupportedOperationException();
             case QUIESCENT:
                 this.setState(State.PAUSED);
-            case STIMULATORY:
-                throw new UnsupportedOperationException();
+                //            case STIMULATORY:
+                //                throw new UnsupportedOperationException();
             default:
                 module = null;
                 break;
