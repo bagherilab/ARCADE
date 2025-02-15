cc910a15b5ab00d0e9f0293424051138a31a10f2
jannetty <jannetty@uw.edu>
2025-02-15 15:05:05 -0800

    removed wildcard import

--- src/arcade/potts/agent/cell/PottsCellFlyNeuron.java
+++ src/arcade/potts/agent/cell/PottsCellFlyNeuron.java
@@ -4,7 +4,7 @@ import ec.util.MersenneTwisterFast;
 import arcade.core.agent.cell.CellState;
 import arcade.core.env.location.Location;
 import arcade.core.util.Parameters;
-import arcade.potts.agent.module.*;
+import arcade.potts.agent.module.PottsModuleProliferationSimple;
 import arcade.potts.util.PottsEnums.State;
 
 /**
