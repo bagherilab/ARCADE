7ab7cab245173442b94384e09a0242ea29415d37
allison-li-1016 <allli@uw.edu>
2025-02-19 21:01:12 +0000

    adding javadoc comments

--- src/arcade/patch/agent/process/PatchProcessQuorumSensing.java
+++ src/arcade/patch/agent/process/PatchProcessQuorumSensing.java
@@ -3,20 +3,34 @@ package arcade.patch.agent.process;
 import java.util.List;
 import arcade.patch.agent.cell.PatchCell;
 
+/**
+ * Implementation of {@link Process} for quorum sensing type modules in which auxin is taken up and
+ * cytotoxic/stimulatory functions are modified.
+ *
+ * <p>The {@code PatchProcessQuorumSensing} module represents an auxin signaling network.
+ */
 public abstract class PatchProcessQuorumSensing extends PatchProcess {
 
-    /** List of amounts of each species */
+    /** List of amounts of each species. */
     protected double[] concs;
 
-    /** List of internal names */
+    /** List of internal names. */
     protected List<String> names;
 
-    /** Number of steps per second to take in ODE */
+    /** Number of steps per second to take in ODE. */
     private static final double STEP_DIVIDER = 3.0;
 
-    /** Step size for module (in seconds) */
+    /** Step size for module (in seconds). */
     static final double STEP_SIZE = 1.0 / STEP_DIVIDER;
 
+    /**
+     * Creates an {@code PatchCellQuorumSensing} module for the given {@link PatchCell}.
+     *
+     * <p>Module parameters are specific for the cell population. The module starts with no internal
+     * auxin. Daughter cells split amounts of bound auxin and receptors upon dividing.
+     *
+     * @param cell the {@link PatchCell} the module is associated with
+     */
     PatchProcessQuorumSensing(PatchCell cell) {
         super(cell);
     }
