1d7fae71985de435e30843cc98374dfdf078eb94
jannetty <jannetty@uw.edu>
2025-02-21 11:35:58 -0800

    formatting fix

--- test/arcade/potts/agent/module/PottsModuleFlyNeuronQuiescenceTest.java
+++ test/arcade/potts/agent/module/PottsModuleFlyNeuronQuiescenceTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class PottsModuleFlyNeuronQuiescenceTest {
-    
+
     @Test
     public void constructor_called_createsInstance() {
         PottsCell cell = mock(PottsCell.class);
         PottsModuleFlyNeuronQuiescence module = new PottsModuleFlyNeuronQuiescence(cell);
         assertNotNull(module);
-        assert(module.getPhase() == Phase.UNDEFINED);
+        assert (module.getPhase() == Phase.UNDEFINED);
     }
 }
