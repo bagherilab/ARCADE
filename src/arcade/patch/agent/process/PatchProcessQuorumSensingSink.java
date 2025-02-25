2180348c8c29691d184130f08c8257ee27d11b60
allison-li-1016 <allli@uw.edu>
2025-02-25 00:44:37 +0000

    adding parameter values for antigens

--- src/arcade/patch/agent/process/PatchProcessQuorumSensingSink.java
+++ src/arcade/patch/agent/process/PatchProcessQuorumSensingSink.java
@@ -129,10 +129,10 @@ public class PatchProcessQuorumSensingSink extends PatchProcessQuorumSensing {
         if (concs[ACTIVATION] > ACTIVATION_THRESHOLD) {
             ((PatchCellCART) cell).setActivationStatus(true);
             ((PatchCellCART) cell).resetLastActiveTicker();
-             ((PatchCellCART) cell).setActivationStatus(true);
-             ((PatchCellCART) cell).resetLastActiveTicker();
+            ((PatchCellCART) cell).setActivationStatus(true);
+            ((PatchCellCART) cell).resetLastActiveTicker();
         } else {
-             ((PatchCellCART) cell).setActivationStatus(false);
+            ((PatchCellCART) cell).setActivationStatus(false);
         }
 
         // update binding status per current tick
