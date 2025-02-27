f6703fda4e202a442607a25081e5c900327dbddd
cainja <jcain@uw.edu>
2025-02-27 07:13:42 +0000

    added modules and tests to confirm state behavior

---
+++ src/arcade/patch/agent/module/PatchModuleSenescence.java
@@ -0,0 +1,19 @@
+package arcade.patch.agent.module;
+
+import ec.util.MersenneTwisterFast;
+import arcade.core.sim.Simulation;
+import arcade.patch.agent.cell.PatchCell;
+
+public class PatchModuleSenescence extends PatchModule {
+    /**
+     * Creates a senescence {@code Module} for the given {@link PatchCell}.
+     *
+     * @param cell the {@link PatchCell} the module is associated with
+     */
+    public PatchModuleSenescence(PatchCell cell) {
+        super(cell);
+    }
+
+    @Override
+    public void step(MersenneTwisterFast random, Simulation sim) {}
+}
