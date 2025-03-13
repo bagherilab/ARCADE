c4138e8cbf4b4671b8cf1d1ea60fd9ec09cd4996
jannetty <jannetty@uw.edu>
2025-03-12 22:04:29 -0700

    renamed exception

--- src/arcade/core/util/exceptions/InvalidParameterValueException.java
+++ src/arcade/core/util/exceptions/InvalidParameterValueException.java
@@ -1,14 +1,14 @@
 package arcade.core.util.exceptions;
 
 /** Exception thrown when parameter value is not equal to single expected value. */
-public class IncorrectlySetParameterException extends RuntimeException {
+public class InvalidParameterValueException extends RuntimeException {
     /**
      * Constructs an {@code InvalidParameterException} with the specified detail message.
      *
      * @param given the given parameter value
      * @param expected the expected parameter value
      */
-    public IncorrectlySetParameterException(double given, double expected) {
+    public InvalidParameterValueException(double given, double expected) {
         super(String.format("Parameter value [ %f ] must be equal to [ %f ]", given, expected));
     }
 }
