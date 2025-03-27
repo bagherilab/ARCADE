c0d1d431784b9e34e73ee151c6ae2b3bef4a01ab
Allison Li <63334013+allison-li-1016@users.noreply.github.com>
2025-03-27 13:34:53 -0700

    CARCADE Refactor 2a: CD8 Cell Agent and Associated Inflammation Process (#160)
    * initial push of files
    
    * addressing linting issues
    
    * adding javadoc and changing variable name to inflammation class
    
    * adding module and inflammation parameters
    
    * adding in new tests for PatchCellCART methods
    
    * adding cd8 tests
    
    * linting the cd8 tests
    
    * adding inflammation cd8 tests
    
    * adding cytotoxic tests
    
    * linting
    
    * better variable names for this class
    
    * better variable names all around
    
    * just importing state and domain instead of entire patch enums class
    
    * getting random double for bound range, not int
    
    * removing redundant max density field
    
    * editing cd8 tests to mock random object
    
    * making binding time a uniform distribution
    
    * addressing merge conflicts
    
    * fixing setState such that CART cells cannot be quiescent
    
    * updating divisions to increment after #161 merge
    
    * making a constant for minutes in a day so that it is more readable
    
    * correctly declaring a final constant
    
    * accidentally deleted the equals sign in the inequality, putting it back
    
    * renaming constant bc its actually minutes in a day
    
    * removing frivolous else statement
    
    * adding description for granzyme synthesis delay param
    
    * making bound time a constant bc the range default was 0
    
    * deleting cell state exception
    
    * alter cd8 test to accomodate for division potential counting up
    
    * setting state to senescent when division potential is met
    
    * changing setup such that divisions cut up
    
    * adding test for BOTH senescence and apoptosis when division potential is met
    
    * renaming tests to match convention

--- src/arcade/patch/agent/process/PatchProcessInflammation.java
+++ src/arcade/patch/agent/process/PatchProcessInflammation.java
@@ -124,14 +124,14 @@ public abstract class PatchProcessInflammation extends PatchProcess {
      * bound and no three-chain receptors. Daughter cells split amounts of bound IL-2 and
      * three-chain receptors upon dividing.
      *
-     * @param c the {@link PatchCellCART} the module is associated with
+     * @param cell the {@link PatchCellCART} the module is associated with
      */
-    public PatchProcessInflammation(PatchCellCART c) {
-        super(c);
-        this.loc = c.getLocation();
-        this.cell = c;
-        this.pop = c.getPop();
-        this.volume = c.getVolume();
+    public PatchProcessInflammation(PatchCellCART cell) {
+        super(cell);
+        this.loc = cell.getLocation();
+        this.cell = cell;
+        this.pop = cell.getPop();
+        this.volume = cell.getVolume();
         this.iL2Ticker = 0;
         this.activeTicker = 0;
 
@@ -311,7 +311,7 @@ public abstract class PatchProcessInflammation extends PatchProcess {
             case "CD4":
                 throw new UnsupportedOperationException();
             case "CD8":
-                throw new UnsupportedOperationException();
+                return new PatchProcessInflammationCD8((PatchCellCART) cell);
             default:
                 return null;
         }
