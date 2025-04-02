7411ce9f8d0b353e0dbbe617bbc398908dd79665
jannetty <jannetty@uw.edu>
2025-04-02 10:29:43 -0700

    wip code for classes

---
+++ src/arcade/potts/agent/cell/PottsCellFlyStem.java
@@ -0,0 +1,102 @@
+package arcade.potts.agent.cell;
+
+import ec.util.MersenneTwisterFast;
+import arcade.core.agent.cell.CellState;
+import arcade.core.env.location.Location;
+import arcade.core.util.GrabBag;
+import arcade.core.util.Parameters;
+import arcade.potts.agent.module.*;
+import arcade.potts.util.PottsEnums.Direction;
+import arcade.potts.util.PottsEnums.State;
+
+/** Extension of {@link PottsCell} for fly stem cells. */
+public class PottsCellFlyStem extends PottsCell {
+
+    /** Enum outlining parameters for each cell type */
+    public enum StemType {
+        WT(50, 80, Direction.ZX_PLANE, 0.2),
+        MUDMUT(50, 50, Direction.YZ_PLANE, 0.5);
+
+        /** Percentage x offset from cell edge where division will occur */
+        public final int splitOffsetPercentX;
+
+        /** Percentage y offset from cell edge where division will occur */
+        public final int splitOffsetPercentY;
+
+        /** Direction of division */
+        public final Direction splitDirection;
+
+        /**
+         * The proportion of the stem cell's critical volume that will be the daughter cell's
+         * critical volume.
+         */
+        public final double daughterCellCriticalVolumeProportion;
+
+        // Constructor
+        StemType(
+                int splitOffsetPercentX,
+                int splitOffsetPercentY,
+                Direction splitDirection,
+                double daughterCellCriticalVolumeProportion) {
+            this.splitOffsetPercentX = splitOffsetPercentX;
+            this.splitOffsetPercentY = splitOffsetPercentY;
+            this.splitDirection = splitDirection;
+            this.daughterCellCriticalVolumeProportion = daughterCellCriticalVolumeProportion;
+        }
+    }
+
+    public final StemType stemType;
+
+    public PottsCellFlyStem(
+            PottsCellContainer container, Location location, Parameters parameters, GrabBag links) {
+        super(container, location, parameters, links);
+        String stemTypeString = parameters.getString("CLASS");
+        switch (stemTypeString) {
+            case "fly-stem-wt":
+                stemType = StemType.WT;
+                break;
+            case "fly-stem-mudmut":
+                stemType = StemType.MUDMUT;
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown StemType: " + stemTypeString);
+        }
+    }
+
+    @Override
+    public PottsCellContainer make(int newID, CellState newState, MersenneTwisterFast random) {
+        divisions++;
+
+        int newPop = links == null ? pop : links.next(random);
+
+        double daughterCellCriticalVolume =
+                criticalVolume * stemType.daughterCellCriticalVolumeProportion;
+
+        return new PottsCellContainer(
+                newID,
+                id,
+                newPop,
+                age,
+                divisions,
+                newState,
+                null,
+                0,
+                null,
+                daughterCellCriticalVolume,
+                criticalHeight,
+                criticalRegionVolumes,
+                criticalRegionHeights);
+    }
+
+    @Override
+    void setStateModule(CellState newState) {
+        switch ((State) newState) {
+            case PROLIFERATIVE:
+                module = new PottsModuleProliferationFlyStem(this);
+                break;
+            default:
+                module = null;
+                break;
+        }
+    }
+}
---
+++ src/arcade/potts/agent/module/PottsModuleProliferationFlyStem.java
@@ -0,0 +1,165 @@
+package arcade.potts.agent.module;
+
+import java.util.ArrayList;
+import sim.util.Double3D;
+import ec.util.MersenneTwisterFast;
+import arcade.core.agent.cell.CellContainer;
+import arcade.core.env.location.Location;
+import arcade.core.sim.Simulation;
+import arcade.core.util.Parameters;
+import arcade.core.util.Plane;
+import arcade.core.util.Vector;
+import arcade.core.util.distributions.NormalDistribution;
+import arcade.potts.agent.cell.PottsCell;
+import arcade.potts.agent.cell.PottsCellFlyStem;
+import arcade.potts.agent.cell.PottsCellFlyStem.StemType;
+import arcade.potts.env.location.PottsLocation;
+import arcade.potts.env.location.PottsLocation2D;
+import arcade.potts.env.location.Voxel;
+import arcade.potts.sim.Potts;
+import arcade.potts.sim.PottsSimulation;
+import arcade.potts.util.PottsEnums.Direction;
+import static arcade.potts.util.PottsEnums.State;
+
+/**
+ * Extension of {@link PottsModuleProliferationSimple} with a custom addCell method for fly stem
+ * cell behavior.
+ */
+public class PottsModuleProliferationFlyStem extends PottsModuleProliferationSimple {
+
+    /** Distribution that determines rotational offset of cell's division plane. */
+    final NormalDistribution splitDirectionDistribution;
+
+    /** Ruleset for determining which daughter cell is the GMC. Can be `volume` or `location`. */
+    final String differentiationRuleset;
+
+    /**
+     * Creates a simple proliferation {@code Module} for the given {@link PottsCellFlyStem}.
+     *
+     * @param cell the {@link PottsCellFlyStem} the module is associated with
+     */
+    public PottsModuleProliferationFlyStem(PottsCellFlyStem cell) {
+        super(cell);
+        Parameters parameters = cell.getParameters();
+
+        splitDirectionDistribution =
+                (NormalDistribution)
+                        parameters.getDistribution("proliferation/DIV_ROTATION_DISTRIBUTION");
+        differentiationRuleset = parameters.getString("proliferation/DIFFERENTIATION_RULESET");
+    }
+
+    /**
+     * Gets the voxel location where the cell will split
+     *
+     * @param cell
+     * @return the voxel location where the cell will split
+     */
+    public static Voxel getCellSplitLocation(PottsCellFlyStem cell) {
+        ArrayList<Integer> splitOffsetPercent = new ArrayList<>();
+        splitOffsetPercent.add(cell.stemType.splitOffsetPercentX);
+        splitOffsetPercent.add(cell.stemType.splitOffsetPercentY);
+        return ((PottsLocation) cell.getLocation()).getOffset(splitOffsetPercent);
+    }
+
+    /**
+     * Gets the division plane for the cell after rotating the plane according to
+     * splitDirectionDistribution.
+     *
+     * @param cell the {@link PottsCellFlyStem} to get the division plane for
+     * @return the division plane for the cell
+     */
+    public Plane getDivisionPlaneWithRotationalVariance(PottsCellFlyStem cell) {
+        Vector plainSplitNormal = cell.stemType.splitDirection.vector;
+        Vector rotatedNormalVector =
+                Vector.rotateVectorAroundAxis(
+                        plainSplitNormal,
+                        Direction.XY_PLANE.vector,
+                        getDivisionPlaneRotationOffset());
+        return new Plane(
+                new Double3D(
+                        getCellSplitLocation(cell).x,
+                        getCellSplitLocation(cell).y,
+                        getCellSplitLocation(cell).z),
+                rotatedNormalVector);
+    }
+
+    /**
+     * Gets the rotation offset for the division plane according to splitDirectionDistribution.
+     *
+     * @return the rotation offset for the division plane
+     */
+    double getDivisionPlaneRotationOffset() {
+        return splitDirectionDistribution.nextDouble();
+    }
+
+    public static PottsLocation getSmallerLocation(
+            PottsLocation location1, PottsLocation location2) {
+        if (location1.getVolume() < location2.getVolume()) {
+            return location1;
+        } else {
+            return location2;
+        }
+    }
+
+    private PottsLocation getBasallLocation(PottsLocation location1, PottsLocation location2) {
+        double[] centroid1 = location1.getCentroid();
+        double[] centroid2 = location2.getCentroid();
+        if (centroid1[1] < centroid2[1]) {
+            return location1;
+        } else {
+            return location2;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The addCell method is overridden to implement specific behavior for fly stem cells in the
+     * proliferation process. Parameters are passed to the {@link PottsLocation2D#split} method
+     * according to the {@link PottsCellFlyStem} {@link StemType}
+     */
+    @Override
+    void addCell(MersenneTwisterFast random, Simulation sim) {
+        Potts potts = ((PottsSimulation) sim).getPotts();
+
+        PottsCellFlyStem flyStemCell = (PottsCellFlyStem) cell;
+        Plane divisionPlane = getDivisionPlaneWithRotationalVariance(flyStemCell);
+
+        // Split current location
+        PottsLocation daughterLoc =
+                (PottsLocation) ((PottsLocation2D) cell.getLocation()).split(random, divisionPlane);
+        PottsLocation stemLoc = (PottsLocation) cell.getLocation();
+
+        Location gmcLoc = null;
+
+        // logic to determine which location should be with which cell
+        if (differentiationRuleset.equals("volume")) {
+            gmcLoc = getSmallerLocation(daughterLoc, stemLoc);
+        } else if (differentiationRuleset.equals("location")) {
+            gmcLoc = getBasallLocation(daughterLoc, stemLoc);
+        } else {
+            throw new IllegalArgumentException(
+                    "Invalid differentiation ruleset: " + differentiationRuleset);
+        }
+
+        // if the gmc location is currently assigned to parent cell, swap the voxels
+        // with the daughter cell location
+        if (stemLoc == gmcLoc) {
+            ((PottsLocation) cell.getLocation())
+                    .swapVoxels(daughterLoc); // swaps the voxels of the two locations
+        }
+
+        // Reset current cell
+        cell.reset(potts.ids, potts.regions);
+
+        // Create and schedule new cell
+        int newID = sim.getID();
+        CellContainer newContainer = cell.make(newID, State.PROLIFERATIVE, random);
+        PottsCell newCell =
+                (PottsCell) newContainer.convert(sim.getCellFactory(), daughterLoc, random);
+        sim.getGrid().addObject(newCell, null);
+        potts.register(newCell);
+        newCell.reset(potts.ids, potts.regions);
+        newCell.schedule(sim.getSchedule());
+    }
+}
