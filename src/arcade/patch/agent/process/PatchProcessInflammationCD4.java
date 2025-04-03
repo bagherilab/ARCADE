d17c33672f8eb5d94c96f7738a09fa3d5d41c8bc
cainja <jcain@uw.edu>
2025-04-02 20:04:19 -0700

    i swear pt2

--- src/arcade/patch/agent/process/PatchProcessInflammationCD4.java
+++ src/arcade/patch/agent/process/PatchProcessInflammationCD4.java
@@ -20,7 +20,7 @@ public class PatchProcessInflammationCD4 extends PatchProcessInflammation {
      * Rate of IL-2 production due to antigen-induced activation [molecules
      * IL-2/cell/min].
      */
-    @SuppressWarnings("memberName")
+    @SuppressWarnings("MemberName")
     private final double IL2ProdRateActive = 293.27;
 
     /** Rate of IL-2 production due to IL-2 feedback [molecules IL-2/cell/min]. */
