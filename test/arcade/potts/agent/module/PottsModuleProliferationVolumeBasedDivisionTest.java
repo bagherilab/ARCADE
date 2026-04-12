3e8bebc1af88d046b7b52ce1842539af4273af06
jannetty <jannetty@uw.edu>
2026-04-11 22:38:57 -0700

    puppycrawl now wants me to undo the odd spacing it asked for

--- test/arcade/potts/agent/module/PottsModuleProliferationVolumeBasedDivisionTest.java
+++ test/arcade/potts/agent/module/PottsModuleProliferationVolumeBasedDivisionTest.java
@@ -17,7 +17,6 @@ public class PottsModuleProliferationVolumeBasedDivisionTest {
 
         boolean addCellCalled = false;
 
-
         boolean growthRateUpdated = false;
 
         PottsModuleProliferationVolumeBasedDivisionMock(PottsCellFlyGMC cell) {
