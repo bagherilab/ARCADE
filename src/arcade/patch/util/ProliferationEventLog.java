73ef234ce5fe6f77be226c2bc8a808591c990970
navyacodes <navyaj17@gmail.com>
2026-05-18 09:51:02 -0700

    fixed typo

--- src/arcade/patch/util/ProliferationEventLog.java
+++ src/arcade/patch/util/ProliferationEventLog.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import arcade.core.util.EventLog;
 
 /**
- * Concrete implementation of {@link EventLog} for lysis events.
+ * Concrete implementation of {@link EventLog} for proliferation events.
  *
  * <p>{@code ProliferationEventLog} objects are created when a cell successfully completes its cell
  * cycle and divides, capturing the ID of the dividing cell and the duration of its cell cycle, as
