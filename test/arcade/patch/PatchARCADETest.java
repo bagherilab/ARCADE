0d7964510d61d8eb5675e91be0aa4f2e51add325
Alice Fu <alicefu@Alices-MacBook-Pro-2.local>
2026-06-01 01:51:56 -0700

    Added filtering so file comparison test does not run unless called with the fileComparisonTest tag (./gradlew fileComparisonTest), and runs after two or more approvals have been given to a pull request

--- test/arcade/patch/PatchARCADETest.java
+++ test/arcade/patch/PatchARCADETest.java
@@ -3,6 +3,8 @@ package arcade.patch;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import com.google.gson.JsonArray;
@@ -117,6 +119,7 @@ public class PatchARCADETest {
     }
 
     @Test
+    @Tag("fileComparison")
     public void main_noVis_fileComparison(@TempDir Path path) throws Exception {
         // Expects an input file at input/[name].xml and expected output files in
         // expected/[name]-expected
